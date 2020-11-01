3891c4b4dd9d42969aac9fb9918a43a60feaa873
scepticae <samuel_leonard@comcast.net>
2020-11-01 02:55:43 -0500

    Level 4 Security: Modified hashing to include salting using bcrypt hashing.

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -21,10 +21,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-
-//Encrypts the secret string with mongoose.encryption
-// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 //Creates new mongodb model for schema
 const User = new mongoose.model("User", userSchema);
@@ -43,24 +39,26 @@ app.get("/register", function(req, res){
 
 //Creates the post to register route
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save(function(err){
-        if(!err){
-            res.render("secrets");
-        }
-        else{
-            console.log(err);
-        }
-    });
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function (err) {
+            if (!err) {
+                res.render("secrets");
+            }
+            else {
+                console.log(err);
+            }
+        });
+    });  
 });
 
 //Creates post to login route and validates username and password
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     //Check for matching username and password
     User.findOne({email: username}, function(err, found){
@@ -69,12 +67,14 @@ app.post("/login", function(req, res){
         }
         else{
             if(found){
-                if(found.password === password){
-                    res.render("secrets");
-                }
-                else{
-                    res.send("That username or password does not exist");
-                }
+                bcrypt.compare(password, found.password, function(err, result){
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                    else{
+                        res.send("The username ore password does not exist.");
+                    }
+                });
             }
             else{
                 res.send("That username or password does not exist");
